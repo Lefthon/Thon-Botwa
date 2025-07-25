2085676199d644e1c3dfcc6420f9011c329de74f
Lefthon <xsanao.gt@gmail.com>
2025-07-26 05:46:57 +0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,26 @@
+module.exports = {
+    // Bot settings
+    name: 'Thon',
+    prefix: '!',
+    sessionPath: './session',
+    
+    // Owner information
+    owner: {
+        number: '6282298334109',
+        name: 'Bot Owner'
+    },
+    
+    // Auto response keywords
+    autoResponses: {
+        'halo': 'Halo juga!',
+        'p': 'P juga',
+        'test': 'Bot aktif!'
+    },
+    
+    // Feature toggles
+    features: {
+        autoDownload: true,
+        antiCall: true,
+        autoSticker: true
+    }
+};
